79fea8dce44c6fd7da78ad20c8c8fde476cd2074
tarekalsaleh <60835161+tarekalsaleh@users.noreply.github.com>
2020-02-09 18:32:54 +1100

    Initial commit

---
+++ Chicago_Public_Transport_Optimization/consumers/models/weather.py
@@ -0,0 +1,21 @@
+"""Contains functionality related to Weather"""
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Weather:
+    """Defines the Weather model"""
+
+    def __init__(self):
+        """Creates the weather model"""
+        self.temperature = 70.0
+        self.status = "sunny"
+
+    def process_message(self, message):
+        
+        """Handles incoming weather data"""
+        data = message.value()
+        self.temperature = data["temperature"]
+        self.status = data["status"]
